ddd1457901f97f4f5f51afb877809b8f793d418d
Laura Daniela Estrada Ibarra <daniela.es.ib@gmail.com>
2025-01-21 13:39:05 -0600

    postgres database config

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,13 @@
-export class Category {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Category extends BaseEntity {
+  /**
+   * BaseEntity is a class that has some built-in methods that help us interact with the database
+   * Active Record pattern is a design pattern that allows us to interact with the database using objects
+   */
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 60 })
+  name: string;
+}
