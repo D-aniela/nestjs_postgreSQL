ea0044d2a44123c1b978fc71c5e2dfd39e88ff8d
Laura Daniela Estrada Ibarra <daniela.es.ib@gmail.com>
2025-01-23 22:49:55 -0600

    add seeder for project

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // This will remove any extra fields that are not defined in the DTO
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.useStaticAssets(join(__dirname, '../public'));
+  app.useStaticAssets(join(__dirname, '..public'));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
