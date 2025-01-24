e880caa2546c384f8c99b489e979ddc6d4645af0
Laura Daniela Estrada Ibarra <daniela.es.ib@gmail.com>
2025-01-23 21:27:30 -0600

    coupons crud

---
+++ src/coupons/coupons.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCouponDto } from './dto/create-coupon.dto';
+import { UpdateCouponDto } from './dto/update-coupon.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Coupon } from './entities/coupon.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CouponsService {
+  constructor(
+    @InjectRepository(Coupon)
+    private readonly couponRepository: Repository<Coupon>,
+  ) {}
+
+  create(createCouponDto: CreateCouponDto) {
+    return this.couponRepository.save(createCouponDto);
+  }
+
+  findAll() {
+    return this.couponRepository.find();
+  }
+
+  async findOne(id: number) {
+    const coupon = await this.couponRepository.findOneBy({ id });
+    if (!coupon) {
+      throw new NotFoundException(`El cupón: #${id} no existe`);
+    }
+    return coupon;
+  }
+
+  async update(id: number, updateCouponDto: UpdateCouponDto) {
+    const coupon = await this.findOne(id);
+    Object.assign(coupon, updateCouponDto);
+    return this.couponRepository.save(coupon);
+  }
+
+  async remove(id: number) {
+    const coupon = await this.findOne(id);
+    await this.couponRepository.remove(coupon);
+    return { message: `El cupón ha sido eliminado` };
+  }
+}
